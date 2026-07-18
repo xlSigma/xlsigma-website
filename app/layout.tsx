d7412950f47e82610b23dda7a3e81bbdd5ec9847
Andres Slack <andresslack@xlsigma.com>
2026-07-17 22:14:15 -0400

    Shorten tab title to xlSigma

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from './components/Footer';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'xlSigma LLC | Management Consulting & Technology',
+  title: 'xlSigma',
   description:
     'Senior-level management consulting and technology services. ' +
     'Process improvement, business process automation, strategy deployment, advanced analytics. ' +
